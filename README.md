94770bc05f41e3b86472d3c6eb8512a5107d32d6
frappe <frappe@datasoft-02>
2023-10-11 19:59:33 +0300

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Sap
+
+ds
+
+#### License
+
+MIT
---
+++ sap/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Sap",
+			"type": "module",
+			"label": _("Sap")
+		}
+	]
